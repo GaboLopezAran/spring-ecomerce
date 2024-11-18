94aaec5d6d20b3f81a5d46eada72340f8bff10df
GaboLópezAran <150459446+GaboLopezAran@users.noreply.github.com>
2024-11-17 20:21:00 -0500

    Crear las clases model, mapeo de clases a tablas con JPA y por ultimo integracion de plantilla home

--- src/main/java/com/ecomerce/demo/model/Orden.java
+++ src/main/java/com/ecomerce/demo/model/Orden.java
@@ -2,13 +2,31 @@ package com.ecomerce.demo.model;
 
 import java.util.Date;
 
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.ManyToOne;
+import jakarta.persistence.OneToOne;
+import jakarta.persistence.Table;
+
+@Entity
+@Table(name="ordenes")
 public class Orden {
 	
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private Integer id;
 	private String numero;
 	private Date fechaCreacion;
 	private Date fechaRecibida;
 	private double total;
+	
+	@ManyToOne
+	private Usuario usuario;
+	
+	@OneToOne(mappedBy = "orden")
+	private DetalleOrden detalleOrden;
 	
 	public Orden(Integer id, String numero, Date fechaCreacion, Date fechaRecibida, double total) {
 		super();
@@ -51,6 +69,21 @@ public class Orden {
 	public void setTotal(double total) {
 		this.total = total;
 	}
+	
+	public Usuario getUsuario() {
+		return usuario;
+	}
+	public void setUsuario(Usuario usuario) {
+		this.usuario = usuario;
+	}
+	
+	public DetalleOrden getDetalleOrden() {
+		return detalleOrden;
+	}
+	public void setDetalleOrden(DetalleOrden detalleOrden) {
+		this.detalleOrden = detalleOrden;
+	}
+	
 	
 	@Override
 	public String toString() {
--- src/main/java/com/ecomerce/demo/model/Usuario.java
+++ src/main/java/com/ecomerce/demo/model/Usuario.java
@@ -1,7 +1,20 @@
 package com.ecomerce.demo.model;
 
+import java.util.List;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.OneToMany;
+import jakarta.persistence.Table;
+
+@Entity
+@Table(name="usuarios")
 public class Usuario {
 	
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private Integer id;
 	private String nombre;
 	private String username;
@@ -11,7 +24,13 @@ public class Usuario {
 	private String tipo;
 	private String password;
 	
+	@OneToMany(mappedBy = "usuario")
+	private List<Producto> productos;
 	
+	@OneToMany(mappedBy = "usuario")
+	private List<Orden>ordenes;
+	
+
 	public Usuario(Integer id, String nombre, String username, String email, String direccion, String telefono,
 			String tipo, String password) {
 		super();
@@ -77,6 +96,15 @@ public class Usuario {
 	public void setPassword(String password) {
 		this.password = password;
 	}
+	
+	
+	public List<Producto> getProductos() {
+		return productos;
+	}
+	public void setProductos(List<Producto> productos) {
+		this.productos = productos;
+	}
+	
 	@Override
 	public String toString() {
 		return "Usuario [id=" + id + ", nombre=" + nombre + ", username=" + username + ", email=" + email
